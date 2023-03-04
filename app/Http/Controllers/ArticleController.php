f12f2c5cbcb0defc4e4d8a3a0b00eda1f04040a3
riv4wi <riv4wi@gmail.com>
2023-03-04 18:16:27 -0300

    feat: Add photos to an article

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Photo;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ArticleController extends Controller
+{
+    public function addPhoto(Request $request, $article_uuid): JsonResponse
+    {
+        $article = Article::findOrFail($article_uuid);
+
+        if ($request->hasFile('photo')) {
+            $file = $request->file('photo');
+            $path = $file->store('api/images');
+
+            $photo = new Photo;
+            $photo->uuid = Str::uuid();
+            $photo->path = $path;
+            $photo->original_name = $file->getClientOriginalName();
+            $photo->article_uuid = $article_uuid;
+            $article->photos()->save($photo);
+
+            return response()->json(['status' => 'success', 'message' => 'Photo added successfully']);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'No photo found']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// To add an image to an article
+Route::post('articles/{article_uuid}/photos', [ArticleController::class, 'addPhoto']);
